37de2c9dc4f52f11b148c646761bf22ae408ca2d
RyoAsai-git <61679701+RyoAsai-git@users.noreply.github.com>
2020-08-25 16:11:22 +0900

    prepare bindParam

--- memo/input_do.php
+++ memo/input_do.php
@@ -25,10 +25,30 @@
         //$dbはオブジェクト PDOのインスタンス
         //$dbのインスタンスが様々なメソッドやプロパティを持っている
 
-        $db->exec('INSERT INTO memos SET memo="' . $_POST['memo'] . '", created_at=NOW()');
+        // $db->exec('INSERT INTO memos SET memo="' . $_POST['memo'] . '", created_at=NOW()');
         //memoの部分の$_POST['memo']はinput.html内のmethodがpostのため
         //postで渡ってきたものにindex.phpのname属性で一致するmemoを指定
         //NOW SQLで利用できる関数 今の時刻をいれる SQLはこのような簡単なプログラムを書くことができる
+        //このコードは危険
+        //$_POSTの値がそのまま指定されている
+        //SQLに渡す文字列はきちんと処理をするべき 危険な文字列や危険な記号が入っているとSQLが壊されデータが盗まれる可能性
+
+        //prepareメソッドを使う
+        $statement = $db->prepare('INSERT INTO memos SET memo=?, created_at=NOW()');
+        $statement->execute(array($_POST['memo']));
+        echo 'メッセージが登録されました';
+
+        //prepare
+        //SQL文のSET memo=?の部分にユーザーが入力した値が入ってくることを準備
+        //$statementはオブジェクトになりexecuteメソッドが準備される
+        //execute
+        //パラメーターに実際に何が入るかの値を指定 ここでいう$_POST['memo']
+        //prepareの作った?に指定した内容が入る //数字か文字かは自動的に判別される
+        //execは完全に安全なSQL つまり固定されたSQLを入力するのには良いが、今回はPOSTの値などformから送信された値をデータベースにいれる場合には必ずprepareで安全性を高める
+        //$statement->bindParam(1, $_POST['memo']);
+        //一番目に$_POST['memo']を指定するというやり方もある
+        //?は複数指定できる 長いSQLの際
+
     } catch (PDOException $e) {
         echo 'DB接続エラー：' . $e->getMessage();
     }
