4530b7a8aaec9c6188518c1561d5a98a4ba9d440
RyoAsai-git <61679701+RyoAsai-git@users.noreply.github.com>
2020-08-27 23:43:11 +0900

    ログイン機能作成

--- mini_bbs/login.php
+++ mini_bbs/login.php
@@ -1,3 +1,41 @@
+<?php 
+    session_start();
+    require('dbconnect.php');
+
+    if (!empty($_POST)) {
+        if ($_POST['email'] !== '' && $_POST['password'] !== '') {
+            //email、passwordが空ではないとき、ログインチェックをする
+            //丁寧に作るのであればメールアドレスが入力されていません、パスワードが入力されていませんと書くべき
+            $login = $db->prepare('SELECT * FROM members WHERE email=? AND password=?');
+            // $login->execute(array($_POST['email'], $_POST['password']));
+            //上の処理は正しく動かない
+            //データベースのpasswordは暗号化 sha1 不可逆暗号 元に戻せない
+            //ここで出力するのはユーザーの生のパスワード
+            //ここのSQLではユーザーの生のパスワードとデータベースの暗号化を比較するため、両者が一致することはないから
+
+            //sha1 ユーザーがパスワードを忘れた際に、元に戻せない //管理者であっても難しい
+            //これを使う意味は?
+            //同じsha1で暗号化した文字は同じ暗号になるというルールがある
+            $login->execute(array($_POST['email'], sha1($_POST['password'])));
+            $member = $login->fetch();
+            //fetchでデータが返ってきていればログインに成功している
+            if ($member) {
+                $_SESSION['id']   = $member['id'];
+                $_SESSION['time'] = time();
+                //セッション変数にはパスワードは保存しないようにする
+                //クッキーより安全性が高いといってもセッションハイジャックという行為でデータ抜き出されるのは不正ログインの原因になる
+                header('Location: index.php');
+                exit;
+            } else {
+                //ログイン失敗処理
+                $error['login'] = 'failed';
+            }
+        } 
+    }
+
+
+?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -18,6 +56,8 @@
       <p>&raquo;<a href="join/">入会手続きをする</a></p>
     </div>
     <form action="" method="post">
+        <!-- action属性が空 -->
+        <!-- login.php自体でエラーがないか確認し、なければ別のページにジャンプする処理を行うため -->
       <dl>
         <dt>メールアドレス</dt>
         <dd>
