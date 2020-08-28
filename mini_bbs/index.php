aafc2bab399df4894984a0b89dc2f0e4dff30d54
RyoAsai-git <61679701+RyoAsai-git@users.noreply.github.com>
2020-08-28 23:13:01 +0900

    画像一覧表示

--- mini_bbs/index.php
+++ mini_bbs/index.php
@@ -16,6 +16,23 @@ if (isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time()) {
   header('Location: login.php');
   exit();
 }
+
+if (!empty($_POST)) {
+    if ($_POST['message'] !== '') {
+        $message = $db->prepare('INSERT INTO posts SET member_id=?, message=?, reply_message_id=0, created=NOW();');
+        $message->execute(array($member['id'], $_POST['message']));
+        //このコードではブラウザで再読み込みした時点でPOSTの値が複数送信されてしまう
+
+        header('Location: index.php');
+        exit;
+        //POSTの処理が終わった後、自分自身をもう一度呼び出す
+        //動きはほとんど変わらない
+    }
+}
+
+$posts = $db->query('SELECT m.name, m.picture, p.* FROM members m, posts p WHERE m.id=p.member_id ORDER BY created DESC');
+//member postsの二つのテーブルをリレーション
+//p.*はpostテーブルの全ての値
 ?>
 
 <!DOCTYPE html>
@@ -51,16 +68,19 @@ if (isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time()) {
       </div>
     </form>
 
+<?php foreach ($posts as $post) : ?>
     <div class="msg">
-    <img src="member_picture" width="48" height="48" alt="" />
-    <p><span class="name">（）</span>[<a href="index.php?res=">Re</a>]</p>
-    <p class="day"><a href="view.php?id="></a>
+    <img src="member_picture/<?php print(htmlspecialchars($post['picture'], ENT_QUOTES)); ?>" width="48" height="48" alt="name" />
+    <!-- alt属性 画像が表示されなかった時に出力されるもの -->
+    <p><?php print(htmlspecialchars($post['message'], ENT_QUOTES)); ?><span class="name">（<?php print(htmlspecialchars($post['name'], ENT_QUOTES)); ?>）</span>[<a href="index.php?res=">Re</a>]</p>
+    <p class="day"><a href="view.php?id="><?php print(htmlspecialchars($post['created'], ENT_QUOTES)); ?></a>
 <a href="view.php?id=">
 返信元のメッセージ</a>
 [<a href="delete.php?id="
 style="color: #F33;">削除</a>]
     </p>
     </div>
+<?php endforeach ?>
 
 <ul class="paging">
 <li><a href="index.php?page=">前のページへ</a></li>
