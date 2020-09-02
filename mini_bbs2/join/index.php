2b983cd0bea4fb20c7350e4e687233d40e28dcff
RyoAsai-git <61679701+RyoAsai-git@users.noreply.github.com>
2020-09-02 22:22:15 +0900

    画像エラーチェック作成

--- mini_bbs2/join/index.php
+++ mini_bbs2/join/index.php
@@ -14,9 +14,19 @@ if (!empty($_POST)) {
     if ($_POST['password'] === '') {
         $error['password'] = 'blank';
     }
+    $fileName = $_FILES['image']['name'];
+    if (!empty($fileName)) {
+        $ext = substr($fileName, - 3);
+        if ($ext != 'jpg' && $ext != 'gif' && $ext != 'png') {
+            $error['image'] = 'type';
+        }
+    }
 
     if (empty($error)) {
-        $_SESSION['join'] = $_POST;
+        $image = date('YmdHis') . $_FILES['image']['name'];
+        move_uploaded_file($_FILES['image']['tmp_name'], '../member_picture/' . $image);
+        $_SESSION['join']          = $_POST;
+        $_SESSION['join']['image'] = $image;
         header('Location: check.php');
         exit;
     }
@@ -58,21 +68,24 @@ if ($_REQUEST['action'] === 'rewrite' && isset($_SESSION['join'])) {
 		<dd>
             <input type="text" name="email" size="35" maxlength="255" value="<?php print(htmlspecialchars($_POST['email'], ENT_QUOTES)) ?>" />
             <?php if ($error['email'] === 'blank') : ?>
-                <p class=error>* メールアドレスを入力してください</p>
+                <p class="error">* メールアドレスを入力してください</p>
             <?php endif ?>
 		<dt>パスワード<span class="required">必須</span></dt>
 		<dd>
             <input type="password" name="password" size="10" maxlength="20" value="<?php print(htmlspecialchars($_POST['password'], ENT_QUOTES)) ?>" />
             <?php if ($error['password'] === 'length') : ?>
-                <p class=error>* パスワードは4文字以上で入力してください</p>
+                <p class="error">* パスワードは4文字以上で入力してください</p>
             <?php endif ?>
             <?php if ($error['password'] === 'blank') : ?>
-                <p class=error>* パスワードを入力してください</p>
+                <p class="error">* パスワードを入力してください</p>
             <?php endif ?>
         </dd>
 		<dt>写真など</dt>
 		<dd>
-        	<input type="file" name="image" size="35" value="test"  />
+            <input type="file" name="image" size="35" value="test"  />
+            <?php if ($error['image'] === 'type') :?>
+                <p class="error">写真などは[.gif]または[.jpg][.png]の画像を指定してください</p>
+            <?php endif ?>
         </dd>
 	</dl>
 	<div><input type="submit" value="入力内容を確認する" /></div>
