0aad28a4036917a17b516ad55cec839973fd34c5
RyoAsai-git <61679701+RyoAsai-git@users.noreply.github.com>
2020-09-02 22:00:19 +0900

    書き直し処理作成

--- mini_bbs2/join/index.php
+++ mini_bbs2/join/index.php
@@ -1,27 +1,30 @@
 <?php
-    session_start();
+session_start();
 
-    if (!empty($_POST)) {
-        if ($_POST['name'] === '') {
-            $error['name'] = 'blank';
-        }
-        if ($_POST['email'] === '') {
-            $error['email'] = 'blank';
-        }
-        if (strlen($_POST['password']) < 4) {
-            $error['password'] = 'length';
-        }
-        if ($_POST['password'] === '') {
-            $error['password'] = 'blank';
-        }
-
-        if (empty($error)) {
-            $_SESSION['join'] = $_POST;
-            header('Location: check.php');
-            exit;
-        }
+if (!empty($_POST)) {
+    if ($_POST['name'] === '') {
+        $error['name'] = 'blank';
+    }
+    if ($_POST['email'] === '') {
+        $error['email'] = 'blank';
+    }
+    if (strlen($_POST['password']) < 4) {
+        $error['password'] = 'length';
+    }
+    if ($_POST['password'] === '') {
+        $error['password'] = 'blank';
     }
 
+    if (empty($error)) {
+        $_SESSION['join'] = $_POST;
+        header('Location: check.php');
+        exit;
+    }
+}
+
+if ($_REQUEST['action'] === 'rewrite' && isset($_SESSION['join'])) {
+    $_POST = $_SESSION['join'];
+}
 ?>
 
 <!DOCTYPE html>
