70ed06f7793793d9461b5613b780becf86793382
RyoAsai-git <61679701+RyoAsai-git@users.noreply.github.com>
2020-09-05 13:26:18 +0900

    ログイン重複登録防止機能

--- mini_bbs2/join/index.php
+++ mini_bbs2/join/index.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
 
+require('../dbconnect.php');
+
 if (!empty($_POST)) {
     if ($_POST['name'] === '') {
         $error['name'] = 'blank';
@@ -20,6 +22,15 @@ if (!empty($_POST)) {
         if ($ext != 'jpg' && $ext != 'gif' && $ext != 'png') {
             $error['image'] = 'type';
         }
+    }
+
+    if (empty($error)) {
+        $member = $db->prepare('SELECT COUNT(*) AS cnt FROM members WHERE email=?');
+        $member->execute(array($_POST['email']));
+        $record = $member->fetch();
+        if ($record['cnt'] > 0) {
+            $error['email'] = 'duplicate';
+        } 
     }
 
     if (empty($error)) {
@@ -69,6 +80,9 @@ if ($_REQUEST['action'] === 'rewrite' && isset($_SESSION['join'])) {
             <input type="text" name="email" size="35" maxlength="255" value="<?php print(htmlspecialchars($_POST['email'], ENT_QUOTES)) ?>" />
             <?php if ($error['email'] === 'blank') : ?>
                 <p class="error">* メールアドレスを入力してください</p>
+            <?php endif ?>
+            <?php if ($error['email'] === 'duplicate') : ?>
+                <p class="error">* 指定されたメールアドレスは既に登録されています</p>
             <?php endif ?>
 		<dt>パスワード<span class="required">必須</span></dt>
 		<dd>
