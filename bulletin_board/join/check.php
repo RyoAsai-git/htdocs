2e7c41b361747313b9299390a7676d45c2f107cc
RyoAsai-git <61679701+RyoAsai-git@users.noreply.github.com>
2020-09-06 13:41:32 +0900

    データベース値追加

--- bulletin_board/join/check.php
+++ bulletin_board/join/check.php
@@ -7,10 +7,20 @@ if (!isset($_SESSION['join'])) {
     exit;
 }
 
-if (!empty($_SESSION['join'])) {
+if (!empty($_POST)) {
     $statement = $db->prepare('INSERT INTO members SET name=?, email=?, password=?, picture=?, created=NOW()');
-    
+    echo $statement->execute(array(
+        $_SESSION['join']['name'],
+        $_SESSION['join']['email'],
+        sha1($_SESSION['join']['password']),
+        $_SESSION['join']['picture'],
+    ));
+
+    unset($_SESSION['join']);
+    header('Location: thanks.php');
+    exit;
 }
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -20,25 +30,29 @@ if (!empty($_SESSION['join'])) {
   <title>Document</title>
 </head>
 <body>
-  <dl>
-    <dt>ニックネーム</dt>
-    <dd><?php print(htmlspecialchars($_SESSION['join']['name'], ENT_QUOTES)) ?></dd>
-  </dl>
-  <dl>
-    <dt>メールアドレス</dt>
-    <dd><?php print(htmlspecialchars($_SESSION['join']['email'], ENT_QUOTES)) ?></dd>
-  </dl>
-  <dl>
-    <dt>パスワード</dt>
-    <dd>パスワードは表示されません</dd>
-  </dl>
-  <dl>
-    <dt>プロフィール画像</dt>
-    <dd>
-      <?php if(!empty($_SESSION['join']['picture'])) : ?>
-        <img src="../member_picture<?php print(htmlspecialchars($_SESSION['join']['picture'], ENT_QUOTES)) ?>" alt="">
-      <?php endif ?>
-    </dd>
-  </dl>
+  <form action="" method="post">
+    <input type="hidden" name="action" value="submit">
+    <dl>
+      <dt>ニックネーム</dt>
+      <dd><?php print(htmlspecialchars($_SESSION['join']['name'], ENT_QUOTES)) ?></dd>
+    </dl>
+    <dl>
+      <dt>メールアドレス</dt>
+      <dd><?php print(htmlspecialchars($_SESSION['join']['email'], ENT_QUOTES)) ?></dd>
+    </dl>
+    <dl>
+      <dt>パスワード</dt>
+      <dd>パスワードは表示されません</dd>
+    </dl>
+    <dl>
+      <dt>プロフィール画像</dt>
+      <dd>
+        <?php if(!empty($_SESSION['join']['picture'])) : ?>
+          <img src="../member_picture<?php print(htmlspecialchars($_SESSION['join']['picture'], ENT_QUOTES)) ?>" alt="">
+        <?php endif ?>
+      </dd>
+    </dl>
+    <input type="submit" value="登録する">
+  </form>
 </body>
 </html>
