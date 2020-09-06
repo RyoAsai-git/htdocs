a752ee5f3395c8695519ffebfe3a030f7fb7a8d1
RyoAsai-git <61679701+RyoAsai-git@users.noreply.github.com>
2020-09-06 17:29:46 +0900

    返信機能

--- bulletin_board/index.php
+++ bulletin_board/index.php
@@ -17,8 +17,8 @@ if (!empty($_POST)) {
         if (!isset($_REQUEST['res'])) {
             $_POST['reply_post_id'] = 0;
         }
-        $message = $db->prepare('INSERT INTO posts SET member_id=?, message=?, reply_message_id=0, created=NOW()');
-        $message->execute(array($member['id'], $_POST['message']));
+        $message = $db->prepare('INSERT INTO posts SET member_id=?, message=?, reply_message_id=?, created=NOW()');
+        $message->execute(array($member['id'], $_POST['message'], $_POST['reply_message_id']));
         header('Location: index.php');
         exit;
     }
@@ -26,6 +26,13 @@ if (!empty($_POST)) {
 
 $posts = $db->prepare('SELECT m.name, m.picture, p.* FROM members m, posts p WHERE m.id=p.member_id ORDER BY created DESC');
 $posts->execute();
+
+if (isset($_REQUEST['res'])) {
+    $response = $db->prepare('SELECT m.name, m.picture, p.* FROM members m, posts p WHERE m.id=p.member_id AND p.id=?');
+    $response->execute(array($_REQUEST['res']));
+    $table = $response->fetch();
+    $message = '@' . $table['name'] . '' . $table['message'];
+}
 
 ?>
 
@@ -41,7 +48,8 @@ $posts->execute();
     <a href="logout.php">ログアウトする</a>
     <dl>
       <dt><?php print(htmlspecialchars($member['name'], ENT_QUOTES)) ?>さん、メッセージをどうぞ</dt>
-      <textarea name="message" id="" cols="50" rows="10"></textarea>
+      <textarea name="message" id="" cols="50" rows="10"><?php print(htmlspecialchars($message, ENT_QUOTES)) ?></textarea>
+      <input type="hidden" name="reply_post_id" value="<?php print(htmlspecialchars($_REQUEST['res'], ENT_QUOTES)) ?>" >
       <input type="submit" value="投稿する">
     </dl>
   </form>
@@ -49,8 +57,11 @@ $posts->execute();
   <?php foreach ($posts as $post) : ?>
     <p><img src="member_picture/<?php print(htmlspecialchars($post['picture'], ENT_QUOTES)) ?>" alt="<?php print(htmlspecialchars($post['name'], ENT_QUOTES)) ?>"></p>
     <p><?php print(htmlspecialchars($post['message'], ENT_QUOTES)) ?></p>
-    <p><a href="view.php?id=<?php print(htmlspecialchars($post['id'], ENT_QUOTES)) ?>"><?php print(htmlspecialchars($post['created'], ENT_QUOTES)) ?></a></p>
-    
+    <p><a href="index.php?res=<?php print(htmlspecialchars($post['id'])) ?>">Re</a></p>
+    <p><a href="view.php?id=<?php print(htmlspecialchars($post['id'], ENT_QUOTES)) ?>"><?php print(htmlspecialchars($post['created'], ENT_QUOTES)) ?>返信元のメッセージ</a></p>
+    <?php if ($post['reply_message_id'] > 0) : ?>
+      <p><a href="view.php?id=<?php print(htmlspecialchars($post['reply_message_id'])) ?>"></a></p>
+    <?php endif ?>
   <?php endforeach ?>
 </body>
 </html>
