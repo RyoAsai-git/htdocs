efecbb3475db3f86cf5ab71e90d29a00ffa94539
RyoAsai-git <61679701+RyoAsai-git@users.noreply.github.com>
2020-09-06 16:36:55 +0900

    メッセージ一覧表示

--- bulletin_board/index.php
+++ bulletin_board/index.php
@@ -1,6 +1,6 @@
 <?php 
-require('dbconnect.php');
 session_start();
+require('dbconnect.php');
 
 if (isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time()) {
     $_SESSION['time'] = time();
@@ -21,7 +21,8 @@ if (!empty($_POST)) {
     }
 }
 
-$posts = $db->prepare('SELECT m.name, m.picture p.* FROM members m, posts p WHERE m.id=p.member_id ORDER BY created DESC');
+$posts = $db->prepare('SELECT m.name, m.picture, p.* FROM members m, posts p WHERE m.id=p.member_id ORDER BY created DESC');
+$posts->execute();
 
 ?>
 
@@ -40,5 +41,11 @@ $posts = $db->prepare('SELECT m.name, m.picture p.* FROM members m, posts p WHER
       <input type="submit" value="投稿する">
     </dl>
   </form>
+
+  <?php foreach ($posts as $post) : ?>
+    <p><img src="member_picture/<?php print(htmlspecialchars($post['picture'], ENT_QUOTES)) ?>" alt="<?php print(htmlspecialchars($post['name'], ENT_QUOTES))?>"></p>
+    <p><?php print(htmlspecialchars($post['message'], ENT_QUOTES)) ?></p>
+    <p><?php print(htmlspecialchars($post['created'], ENT_QUOTES)) ?></p>
+  <?php endforeach ?>
 </body>
 </html>
