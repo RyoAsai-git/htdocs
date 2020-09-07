6b00d7b2c77752d7b70042fea1c3327949742a2b
RyoAsai-git <61679701+RyoAsai-git@users.noreply.github.com>
2020-09-07 22:21:33 +0900

    動的型付けなど

--- php_test/index.php
+++ php_test/index.php
@@ -5,8 +5,17 @@
 
     <?php 
       echo ('こちらはPHP');
-
+      echo ('<br>');
+      echo ('こんにち""は');
       phpinfo();
+
+      //php 動的型付け int stringを自動的に判断  Javaなど静的型付け int() string()と型を指定する必要
+      
+      $test_1 = 123;
+      $test_2 = 456;
+      $test_3 = $test_1 . $test_2;
+      echo $test_3;
+      //string型で出力 二つの変数を繋げるとstringになる
     ?>
   </body>
 </html>
