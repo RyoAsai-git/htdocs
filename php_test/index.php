640422ad7fddf0d6da5018246c0c0348237df80e
RyoAsai-git <61679701+RyoAsai-git@users.noreply.github.com>
2020-09-07 22:37:41 +0900

    データ構造

--- php_test/index.php
+++ php_test/index.php
@@ -1,6 +1,52 @@
-<?php 
-//定数 変わらない数 文字
-const MAX = 10;
-const MAX = 'テスト';
-//上書きされない
-echo MAX;
+<?php
+
+$array = [1,2,3];
+
+echo '<pre>';
+//preで配列を縦に見やすく出力する
+var_dump($array);
+echo '</pre>';
+
+$array_2 = [
+    ['赤','青','黄色'],
+    ['緑', '紫', '黒'],
+];
+
+echo '<pre>';
+var_dump($array_2);
+echo '</pre>';
+
+$array_member_2 = [
+    '本田' => [
+        'height' => 180,
+        'hobby' => 'サッカー',
+    ],
+    '香川' => [
+        'height' => 180,
+        'hobby' => 'サッカー',
+    ]
+];
+
+$array_member_3 = [
+    '1kumi' => [
+        '本田' => [
+            'height' => 180,
+            'hobby' => 'サッカー',
+        ],
+        '香川' => [
+            'height' => 180,
+            'hobby' => 'サッカー',
+        ]
+    ],
+    '2kumi' => [
+        '長友' => [
+        'height' => 180,
+        'hobby' => 'サッカー',
+        ],
+        '乾' => [
+            'height' => 180,
+            'hobby' => 'サッカー',
+        ],
+    ],
+
+];
