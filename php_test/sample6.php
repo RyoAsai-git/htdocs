f00906c50c3baa1ab5b00c4e09fb26fc5183749a
RyoAsai-git <61679701+RyoAsai-git@users.noreply.github.com>
2020-09-09 18:05:57 +0900

    スコープ外変数

--- php_test/sample6.php
+++ php_test/sample6.php
@@ -3,14 +3,23 @@
 
 $globalVariable = 'グローバル変数です';
 
-function checkScope() {
+function checkScope($str) {
     $localVariable = "ローカル変数です";
-    echo $localVariable;
+    // echo $localVariable;
+
+    // echo $globalVariable;
+    //表示されない
+    //スコープ
+    echo $str;
 }
 
-echo $globalVariable;
-echo $localVariable;
+// echo $globalVariable;
+// echo $localVariable;
 
 //このままではスコープの範囲で$localVariableは出力されない
 // checkScope();
 //
+
+checkScope($globalVariable);
+
+//変数をスコープの範囲外で使いたい場合は関数の引数で渡してあげると良い
