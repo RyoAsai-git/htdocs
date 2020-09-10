c12ee3971f4ca57cff593eb920f3de34a9af4fe8
RyoAsai-git <61679701+RyoAsai-git@users.noreply.github.com>
2020-09-10 11:50:33 +0900

    戻るボタン作成

--- php_test/form/input2.php
+++ php_test/form/input2.php
@@ -27,6 +27,9 @@ if (!empty($_POST["btn_submit"])){
       <br>
       メールアドレス
       <?php echo $_POST["email"] ; ?>
+      <input type="submit" name="back" value="戻る">
+      <!-- 戻るが押されるとphpないの処理である$pageFlagが0に戻る -->
+      <!-- $_POST['btn_submit']がからのため empty -->
       <input type="submit" name="btn_submit" value="送信する">
       <input type="hidden" name="your_name" value="<?php echo $_POST["your_name"] ; ?>">
       <input type="hidden" name="email" value="<?php echo $_POST["email"] ; ?>">
@@ -40,10 +43,10 @@ if (!empty($_POST["btn_submit"])){
   <?php if($pageFlag === 0) : ?>
     <form method="POST" action="input2.php">
       名前
-      <input type="text" name="your_name">
+      <input type="text" name="your_name" value="<?php echo $_POST['your_name'] ?>">
       <br>
       メールアドレス
-      <input type="email" name="email">
+      <input type="email" name="email" value="<?php echo $_POST['email'] ?>">
       <input type="submit" name="btn_confirm" value="確認する">
     </form>
   <?php endif; ?>
