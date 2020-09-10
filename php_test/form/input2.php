7db235254c190d01981b84c28ed6100ac5a241db
RyoAsai-git <61679701+RyoAsai-git@users.noreply.github.com>
2020-09-10 16:21:07 +0900

    セキュリティ

--- php_test/form/input2.php
+++ php_test/form/input2.php
@@ -1,8 +1,17 @@
 <?php
+
+header('X-FRAME-OPTIONS:DENY');
+//クリックジャッキング対策
+//重ねて表示はできないことを明示
+
+function h ($str) {
+    // <!-- htmlspecialcharsを省略しh -->
+    return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
+}
  
-echo "<pre>";
-var_dump($_POST);
-echo "</pre>";
+// echo "<pre>";
+// var_dump($_POST);
+// echo "</pre>";
  
 $pageFlag = 0;
  
@@ -23,16 +32,18 @@ if (!empty($_POST["btn_submit"])){
   <?php if($pageFlag === 1) : ?>
     <form method="POST" action="input2.php">
       名前
-      <?php echo $_POST["your_name"] ; ?>
+      <?php echo h($_POST["your_name"]) ; ?>
+      <!-- htmlspecialcharsを省略しh -->
       <br>
       メールアドレス
-      <?php echo $_POST["email"] ; ?>
+      <?php echo h($_POST["email"]) ; ?>
+      <!-- htmlspecialcharsを省略しh -->
       <input type="submit" name="back" value="戻る">
       <!-- 戻るが押されるとphpないの処理である$pageFlagが0に戻る -->
       <!-- $_POST['btn_submit']がからのため empty -->
       <input type="submit" name="btn_submit" value="送信する">
-      <input type="hidden" name="your_name" value="<?php echo $_POST["your_name"] ; ?>">
-      <input type="hidden" name="email" value="<?php echo $_POST["email"] ; ?>">
+      <input type="hidden" name="your_name" value="<?php echo h($_POST["your_name"]) ; ?>">
+      <input type="hidden" name="email" value="<?php echo h($_POST["email"]) ; ?>">
     </form>
   <?php endif; ?>
   
@@ -43,12 +54,31 @@ if (!empty($_POST["btn_submit"])){
   <?php if($pageFlag === 0) : ?>
     <form method="POST" action="input2.php">
       名前
-      <input type="text" name="your_name" value="<?php echo $_POST['your_name'] ?>">
+      <input type="text" name="your_name" value="<?php echo h($_POST['your_name']) ?>">
       <br>
       メールアドレス
-      <input type="email" name="email" value="<?php echo $_POST['email'] ?>">
+      <input type="email" name="email" value="<?php echo h($_POST['email']) ?>">
       <input type="submit" name="btn_confirm" value="確認する">
     </form>
   <?php endif; ?>
 </body>
 </html>
+
+<!-- formが狙われる代表的な攻撃 -->
+
+<!-- XSS Cross-Site Scripting -->
+  <!--悪意を持った攻撃者 -->
+  <!-- formで<script>alert('攻撃');</alert> -->
+  <!-- JSでできることで攻撃できてしまう サニタイズする htmlspecialcharsを用いる -->
+
+<!-- クリックジャッキング -->
+  <!-- クリックするボタンをのっとる -->
+  <!-- ボタンの上にcssで透明にしたボタンを気づかずにクリックさせる -->
+  <!-- サーバー側とphp側の対策 -->
+  
+
+<!-- CSRF Cross Site Request Forgeries -->
+<!-- SQLインジェクション -->
+
+<!-- 対策 -->
+<!-- サニタイズ バリデーション -->
