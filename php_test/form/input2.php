d783e0a99030878201e2e2143b63a21dc8fb55c7
RyoAsai-git <61679701+RyoAsai-git@users.noreply.github.com>
2020-09-10 17:12:30 +0900

    フォームバリデーション

--- php_test/form/input2.php
+++ php_test/form/input2.php
@@ -12,7 +12,7 @@ function h ($str) {
 }
  
 echo "<pre>";
-var_dump($_SESSION);
+var_dump($_POST);
 echo "</pre>";
  
 $pageFlag = 0;
@@ -35,19 +35,45 @@ if (!empty($_POST["btn_submit"])){
   <!-- 確認画面 -->
     <?php if ($_POST['csrf'] === $_SESSION['csrfToken']) : ?>
       <form method="POST" action="input2.php">
-        名前
+        氏名
         <?php echo h($_POST["your_name"]) ; ?>
         <!-- htmlspecialcharsを省略しh -->
         <br>
         メールアドレス
         <?php echo h($_POST["email"]) ; ?>
         <!-- htmlspecialcharsを省略しh -->
+        <br>
+        ホームページ
+        <?php echo h($_POST['url']) ?>
+        <br>
+        性別
+        <?php if ($_POST['gender'] === '0'){echo '男性';}
+              if ($_POST['gender'] === '1'){echo '女性';} 
+        ?>
+        <br>
+        年齢
+        <?php if ($_POST['age'] === '1'){echo '〜19歳';}
+              if ($_POST['age'] === '2'){echo '20歳〜29歳';}
+              if ($_POST['age'] === '3'){echo '30歳〜39歳';}
+              if ($_POST['age'] === '4'){echo '40歳〜49歳';}
+              if ($_POST['age'] === '5'){echo '50歳〜59歳';}
+              if ($_POST['age'] === '6'){echo '60歳〜';}
+        ?>
+        <br>
+        お問い合わせ内容
+        <?php echo h($_POST['contact']) ?>
+
         <input type="submit" name="back" value="戻る">
         <!-- 戻るが押されるとphpないの処理である$pageFlagが0に戻る -->
         <!-- $_POST['btn_submit']がからのため empty -->
         <input type="submit" name="btn_submit" value="送信する">
         <input type="hidden" name="your_name" value="<?php echo h($_POST["your_name"]) ; ?>">
         <input type="hidden" name="email" value="<?php echo h($_POST["email"]) ; ?>">
+        <input type="hidden" name="url" value="<?php echo h($_POST["url"]) ; ?>">
+        <input type="hidden" name="gender" value="<?php echo h($_POST["gender"]) ; ?>">
+        <input type="hidden" name="age" value="<?php echo h($_POST["age"]) ; ?>">
+        <input type="hidden" name="contact" value="<?php echo h($_POST["contact"]) ; ?>">
+
         <input type="hidden" name="csrf" value="<?php echo h($_POST['csrf']); ?>">
       </form>
     <?php endif ?>
@@ -75,11 +101,37 @@ if (!empty($_POST["btn_submit"])){
 
 
     <form method="POST" action="input2.php">
-      名前
+      氏名
       <input type="text" name="your_name" value="<?php echo h($_POST['your_name']) ?>">
       <br>
       メールアドレス
       <input type="email" name="email" value="<?php echo h($_POST['email']) ?>">
+      <br>
+      ホームページ
+      <input type="url" name="url" value="<?php echo h($_POST['url']) ?>">
+      <br>
+      性別
+      <input type="radio" name="gender" value="0">男性
+      <input type="radio" naem="gender" value="1">女性
+      <br>
+      年齢
+      <select name="age">
+      <option value="">選択してください</option>
+        <option value="1">〜19歳</option>
+        <option value="2">20歳〜29歳</option>
+        <option value="3">30歳〜39歳</option>
+        <option value="4">40歳〜49歳</option>
+        <option value="5">50歳〜59歳</option>
+        <option value="6">60歳〜</option>
+      </select>
+      <br>
+      お問い合わせ内容
+      <textarea name="contact" value="<?php echo h($_POST['contact']) ?>"></textarea>
+      <br>
+      <input type="checkbox" name="caution" value="1">注意事項にチェックする
+      <br>
+
+
       <input type="submit" name="btn_confirm" value="確認する">
       <input type="hidden" name="csrf" value="<?php echo $token ?>">
       <!-- 合言葉を設定 -->
@@ -112,3 +164,10 @@ if (!empty($_POST["btn_submit"])){
 
 <!-- 対策 -->
 <!-- サニタイズ バリデーション -->
+
+<!-- バリデーション -->
+<!-- 文字 未入力 -->
+    <!-- 氏名未入力だと名無しになってしまう 100文字などの現実的な氏名ではない -->
+<!-- メールアドレス 未入力 1つだけか-->
+<!-- 性別などの選択項目 未入力 -->
+<!-- 郵便番号 電話番号 カナ -->
