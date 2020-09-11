887932286a4bfe55df56abacf942a42cea3d977d
RyoAsai-git <61679701+RyoAsai-git@users.noreply.github.com>
2020-09-11 16:34:27 +0900

    bootstrap導入

--- php_test/form/input2.php
+++ php_test/form/input2.php
@@ -30,9 +30,18 @@ if (!empty($_POST["btn_submit"])) {
  
 ?>
  
-<!DOCTYPE html>
-<meta charset="utf-8">
-<head></head>
+ <!doctype html>
+<html lang="ja">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
 <body> 
   <?php if($pageFlag === 1) : ?>
     <!-- 確認画面 -->
@@ -114,9 +123,20 @@ if (!empty($_POST["btn_submit"])) {
       </ul>
     <?php endif ?>
 
-    <form method="POST" action="input2.php">
-      氏名
-      <input type="text" name="your_name" value="<?php echo h($_POST['your_name']) ?>">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+        <!-- bootstrapは画面を12分割して考える -->
+        <!-- ここでは12分割した半分を使っている -->
+        <!-- md 画面幅が720pxより小さくなったら100%で表示する -->
+          <form method="POST" action="input2.php">
+          <div class="form-group">
+            <label for="your_name">氏名 </label>
+            <input type="text" class="form-control" id="your_name" name="your_name" value="<?php echo h($_POST['your_name']) ?>" required>
+            <!-- required 必須項目に記述 -->
+          </div>
+        </div>
+      </div>
       <br>
       メールアドレス
       <input type="email" name="email" value="<?php echo h($_POST['email']) ?>">
@@ -152,6 +172,11 @@ if (!empty($_POST["btn_submit"])) {
       <!-- 合言葉を設定 -->
     </form>
   <?php endif ?>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 </body>
 </html>
 
