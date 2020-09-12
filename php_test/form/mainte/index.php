5b54c2c89a0bd4090180f7794cae172a093b0f96
RyoAsai-git <61679701+RyoAsai-git@users.noreply.github.com>
2020-09-12 16:11:49 +0900

    トランザクション処理

--- php_test/form/mainte/index.php
+++ php_test/form/mainte/index.php
@@ -15,7 +15,6 @@ require('db_connection.php');
 // echo '<pre>';
 
 
-
 //ユーザー入力あり prepare, bind, execute  <- SQLインジェクション対策のためにこの三つを使う 
 //悪意のあるユーザーにお問い合わせにdelete文などを打ってくる データベースが不正に操作される SQLインジェクション
 //検索画面 お問い合わせフォームなどユーザーが入力する場合
@@ -38,3 +37,26 @@ echo '<pre>';
 var_dump($result);
 echo '<pre>';
 
+//トランザクション まとまって処理するときに必要な考え方
+// ex 銀行 残高を確認->Aさんから引き落とし->Bさんに振り込む
+// sqlだとAさんから金額を引いて Bさんに足す
+// 仮にBさんに振り込む際に処理が止まってしまい、Aさんから引き落としているのに、Bさんに振り込まれていないケースが発生してしたとする
+// これは非常にまずいので 残高を確認->Aさんから引き落とし->Bさんに振り込む の処理を一括りにする
+// うまくいかなければ最初に戻る
+
+// beginTransaction トランザクションの開始
+// commit まとめて処理
+// rollback うまくいかなければ最初に戻る
+
+$pdo->beginTransaction();
+
+try {
+    //sql処理
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindValue('id', 3, PDO::PARAM_INT);
+    $stmt->execute();
+    
+    $pdo->commit();
+} catch (PDOException $e) {
+    $pdo->rollBack(); //更新のキャンセル
+}
