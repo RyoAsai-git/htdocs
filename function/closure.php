f031718d220f00f1043fb07340a27849296bfcde
RyoAsai-git <61679701+RyoAsai-git@users.noreply.github.com>
2020-09-13 13:59:37 +0900

    無名関数クロージャ

--- function/closure.php
+++ function/closure.php
@@ -3,6 +3,7 @@
 function useCombine(array $name, callable $func) {
     $concatName = $func(...$name);
     print('無名関数での結合結果:'. $concatName . '<br>');
+    //無名関数での結合結果:苗字 名前
 }
 
 $nameParam = ['名前', '苗字'];
@@ -10,5 +11,6 @@ $nameParam = ['名前', '苗字'];
 //引数に名前のない関数(無名関数(クロージャ))
 useCombine($nameParam, function(string $firstName, string $lastName) {
     return $lastName . " " . $firstName; 
+    //ここでのfunctionはcallableの$funcで呼び出されている?
 }
 );
