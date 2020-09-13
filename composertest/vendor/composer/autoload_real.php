79e4503c5493af97207ada5610ef18913e0eae1e
RyoAsai-git <61679701+RyoAsai-git@users.noreply.github.com>
2020-09-13 17:42:56 +0900

    carbonインストール

--- composertest/vendor/composer/autoload_real.php
+++ composertest/vendor/composer/autoload_real.php
@@ -50,6 +50,24 @@ class ComposerAutoloaderInit8be566899fe5a609efbcc461f9295b6b
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit8be566899fe5a609efbcc461f9295b6b::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire8be566899fe5a609efbcc461f9295b6b($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire8be566899fe5a609efbcc461f9295b6b($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
--- composertest/vendor/composer/autoload_static.php
+++ composertest/vendor/composer/autoload_static.php
@@ -6,7 +6,23 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit8be566899fe5a609efbcc461f9295b6b
 {
+    public static $files = array (
+        '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
+        'a4a119a56e50fbb293281d9a48007e0e' => __DIR__ . '/..' . '/symfony/polyfill-php80/bootstrap.php',
+    );
+
     public static $prefixLengthsPsr4 = array (
+        'S' => 
+        array (
+            'Symfony\\Polyfill\\Php80\\' => 23,
+            'Symfony\\Polyfill\\Mbstring\\' => 26,
+            'Symfony\\Contracts\\Translation\\' => 30,
+            'Symfony\\Component\\Translation\\' => 30,
+        ),
+        'C' => 
+        array (
+            'Carbon\\' => 7,
+        ),
         'A' => 
         array (
             'App\\' => 4,
@@ -14,10 +30,36 @@ class ComposerStaticInit8be566899fe5a609efbcc461f9295b6b
     );
 
     public static $prefixDirsPsr4 = array (
+        'Symfony\\Polyfill\\Php80\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/polyfill-php80',
+        ),
+        'Symfony\\Polyfill\\Mbstring\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/polyfill-mbstring',
+        ),
+        'Symfony\\Contracts\\Translation\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/translation-contracts',
+        ),
+        'Symfony\\Component\\Translation\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/translation',
+        ),
+        'Carbon\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/nesbot/carbon/src/Carbon',
+        ),
         'App\\' => 
         array (
             0 => __DIR__ . '/../..' . '/app',
         ),
+    );
+
+    public static $classMap = array (
+        'Stringable' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/Stringable.php',
+        'UnhandledMatchError' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/UnhandledMatchError.php',
+        'ValueError' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/ValueError.php',
     );
 
     public static function getInitializer(ClassLoader $loader)
@@ -25,6 +67,7 @@ class ComposerStaticInit8be566899fe5a609efbcc461f9295b6b
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit8be566899fe5a609efbcc461f9295b6b::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit8be566899fe5a609efbcc461f9295b6b::$prefixDirsPsr4;
+            $loader->classMap = ComposerStaticInit8be566899fe5a609efbcc461f9295b6b::$classMap;
 
         }, null, ClassLoader::class);
     }
