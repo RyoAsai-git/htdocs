1ce5abb1ee73427ecb5bd9e37f6af7e56afe16f0
RyoAsai-git <61679701+RyoAsai-git@users.noreply.github.com>
2020-09-13 13:54:28 +0900

    コールバック関数

--- function/callback.php
+++ function/callback.php
@@ -9,7 +9,10 @@ $nameParam = ['名前', '苗字'];
 //コールバック関数
 function useCombine(array $name, callable $func) {
     $concatName = $func(...$name);
+    // var_dump($name);
+    //array(2) { [0]=> string(6) "名前" [1]=> string(6) "苗字" }
     print($func . '関数の結合結果:' . $concatName . '<br>');
+    //combineSpace関数の結合結果:苗字名前
 }
 
 
