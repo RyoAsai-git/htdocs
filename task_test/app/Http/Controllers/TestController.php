84b676a17e53f18b6da36191a0ae3d7c6cd53dcd
RyoAsai-git <61679701+RyoAsai-git@users.noreply.github.com>
2020-09-15 17:35:38 +0900

    簡易データベース内容表示

--- task_test/app/Http/Controllers/TestController.php
+++ task_test/app/Http/Controllers/TestController.php
@@ -4,12 +4,78 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Test;
+//作成したModelsファイルのTestを持ってくることができる
+
 class TestController extends Controller
 {
     //
     public function index() {
+
+        $values = Test::all();
+        //変数$valueにモデル名のall
+
+        // dd($values);
+        // ddコマンド
+        // phpでいうdieとvar_dumpをセットにしたもの
+        // 処理を止めて変数の中身を表示してくれる
+
+        //このように変数の中身が表示される
+        //collection型
+    
+        // Illuminate\Database\Eloquent\Collection {#1341 ▼
+        //     #items: array:2 [▼
+        //       0 => App\Models\Test {#1340 ▼
+        //         #connection: "mysql"
+        //         #table: "tests"
+        //         #primaryKey: "id"
+        //         #keyType: "int"
+        //         +incrementing: true
+        //         #with: []
+        //         #withCount: []
+        //         #perPage: 15
+        //         +exists: true
+        //         +wasRecentlyCreated: false
+        //         #attributes: array:4 [▼
+        //           "id" => 1
+        //           "text" => "aaa"
+        //           "created_at" => "2020-09-15 16:54:33"
+        //           "updated_at" => "2020-09-15 16:54:33"
+        //         ]
+        //         #original: array:4 [▼
+        //           "id" => 1
+        //           "text" => "aaa"
+        //           "created_at" => "2020-09-15 16:54:33"
+        //           "updated_at" => "2020-09-15 16:54:33"
+        //         ]
+        //         #changes: []
+        //         #casts: []
+        //         #dates: []
+        //         #dateFormat: null
+        //         #appends: []
+        //         #dispatchesEvents: []
+        //         #observables: []
+        //         #relations: []
+        //         #touches: []
+        //         +timestamps: true
+        //         #hidden: []
+        //         #visible: []
+        //         #fillable: []
+        //         #guarded: array:1 [▶]
+        //       }
+        //       1 => App\Models\Test {#1339 ▶}
+        //     ]
+        //   }
+
+        // $valuesをviewに持っていく
+        // compact関数を使う
+        // 複数の変数も渡すことができる
+
+        return view('tests.test', compact('values'));
+            //文字の変数は''で囲む
+
         //先ほどのweb.php内の命令からここが実行される
-        return view('tests.test');
+        // return view('tests.test');
         //resources/views/tests/に飛ぶ 
         //test.blade.php
     }
--- task_test/resources/views/tests/test.blade.php
+++ task_test/resources/views/tests/test.blade.php
@@ -1,2 +1,20 @@
 test
 <!-- viewファイルは基本的にbladeをつける -->
+
+<!-- TestControllerから渡されたコレクション型の$valuesを表示するためにはforeach -->
+
+@foreach($values as $value)
+{{$value->id}}<br>
+{{$value->text}}<br>
+@endforeach
+
+<!-- 以下のように出力される -->
+<!-- test 1
+aaa
+2
+bbb -->
+
+<!-- ここまでの処理の流れ -->
+<!-- ルーティングでtests/testにアクセス -->
+<!-- コントローラーに渡り、データベースの情報を持ってくる -->
+<!-- データベースの情報をviewでforeachで展開 -->
